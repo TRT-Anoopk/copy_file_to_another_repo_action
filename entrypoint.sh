00a0bda6ba8fa1370e11d1f45a9cef7ea1f1ebf4
TRT-Anoopk <anoop.kaushal@raytheon.co.uk>
2021-04-28 12:26:34 +0100

    reset readme

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,7 +40,7 @@ fi
 
 echo "Adding git commit"
 git add .
-git reset README.md
+git reset -- README
 if git status | grep -q "Changes to be committed"
 then
   git commit --message "$INPUT_COMMIT_MESSAGE"
